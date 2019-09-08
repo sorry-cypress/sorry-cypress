57b4166a4e70d386ab465a17aa41fac80a19ef35
Andrew Goldis <agoldis@gmail.com>
2019-09-07 22:40:49 -0700

    Add example project

---
+++ example/cypress/integration/c.spec.js
@@ -0,0 +1,14 @@
+describe('Suite C', function() {
+  it('Welcomes you', function() {
+    cy.visit('/');
+    cy.wait(10000);
+    cy.contains('Hello and sorry Cypress');
+  });
+
+  it('Shows 404', function() {
+    cy.visit('/non-existing', {
+      failOnStatusCode: false
+    });
+    cy.contains('404');
+  });
+});
