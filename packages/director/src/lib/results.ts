096d6715042ef5812aea1b863ba6b45afd11d063
Andrew Goldis <agoldis@gmail.com>
2020-03-06 21:29:33 -0800

    Sanitize mongodb values. Closes #38.

--- packages/director/src/lib/results.ts
+++ packages/director/src/lib/results.ts
@@ -1,4 +1,9 @@
 import { InstanceResult } from '@src/types';
-
+import { deepTraverseKeys } from './lang';
 export const isInstanceFailed = (results: InstanceResult) =>
   results.stats.failures > 0;
+
+export const getSanitizedMongoObject = (target: object) =>
+  deepTraverseKeys(target, (key: string) =>
+    key.replace('$', '_').replace('.', '_')
+  );
