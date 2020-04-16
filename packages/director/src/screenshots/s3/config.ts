2f8198eac1aba895b0281981653de4fa54bdd2ae
Meet Shah <meet1995@gmail.com>
2020-04-16 11:15:26 +0530

    Fix reading of S3 ACL environment variable

--- packages/director/src/screenshots/s3/config.ts
+++ packages/director/src/screenshots/s3/config.ts
@@ -1,4 +1,4 @@
 export const S3_BUCKET = process.env.S3_BUCKET || 'sorry-cypress';
 export const S3_REGION = process.env.S3_REGION || 'us-east-1';
-export const S3_ACL = process.env.S3_REGION || 'public-read';
+export const S3_ACL = process.env.S3_ACL || 'public-read';
 export const S3_READ_URL_PREFIX = process.env.S3_READ_URL_PREFIX || null;
