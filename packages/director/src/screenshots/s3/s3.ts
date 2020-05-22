9b6660db80914f0ea7b3763317d2e150578187ff
Paul Lesur <paul.lesur@free.fr>
2020-05-22 15:45:12 +0200

    Works with cegedim s3

--- packages/director/src/screenshots/s3/s3.ts
+++ packages/director/src/screenshots/s3/s3.ts
@@ -1,10 +1,6 @@
 import aws from 'aws-sdk';
 import {
-  S3_BUCKET,
   S3_REGION,
-  S3_ACL,
-  S3_ENDPOINT,
-  S3_READ_URL_PREFIX,
   S3_IMAGE_KEY_PREFIX,
   S3_VIDEO_KEY_PREFIX,
 } from './config';
@@ -12,13 +8,14 @@ import { S3SignedUploadResult } from './types';
 import { AssetUploadInstruction } from '@src/types';
 import { sanitizeS3KeyPrefix } from './utils';
 
-const BUCKET_URL = `https://${S3_BUCKET}.${S3_ENDPOINT}`;
 const ImageContentType = 'image/png';
 const VideoContentType = 'video/mp4';
 
 const s3 = new aws.S3({
   region: S3_REGION,
   signatureVersion: 'v4',
+  endpoint: 'storage-eb4.cegedim.cloud',
+  s3ForcePathStyle: true,
 });
 
 interface GetUploadURLParams {
@@ -32,24 +29,28 @@ export const getUploadUrl = async ({
   ContentType = ImageContentType,
   Expires = 60,
 }: GetUploadURLParams): Promise<S3SignedUploadResult> => {
-  const s3Params = {
-    Bucket: S3_BUCKET,
+  const s3ParamsRead = {
+    Bucket: 'sorry-cypress',
     Key: key,
-    Expires: Expires,
-    ContentType: ContentType,
-    // ACL: S3_ACL,
+  };
+
+  const s3ParamsUpload = {
+    Expires,
+    ContentType,
+    ...s3ParamsRead
   };
 
   return new Promise((resolve, reject) => {
+    const signedReadURL = s3.getSignedUrl('getObject', s3ParamsRead);
     s3.getSignedUrl(
       'putObject',
-      s3Params,
+      s3ParamsUpload,
       (error: Error, uploadUrl: string) => {
         if (error) {
           return reject(error);
         }
         return resolve({
-          readUrl: `${S3_READ_URL_PREFIX || BUCKET_URL}/${key}`,
+          readUrl: signedReadURL,
           uploadUrl,
         });
       }
