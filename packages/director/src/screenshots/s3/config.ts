ddbd38011887ce3ae067ae964ef00272ef502008
Paul Lesur <paul.lesur@free.fr>
2020-06-09 16:02:39 +0200

    Expires for url read

--- packages/director/src/screenshots/s3/config.ts
+++ packages/director/src/screenshots/s3/config.ts
@@ -5,4 +5,4 @@ export const S3_ENDPOINT = process.env.S3_ACL || 's3.amazonaws.com';
 export const S3_READ_URL_PREFIX = process.env.S3_READ_URL_PREFIX || null;
 export const S3_IMAGE_KEY_PREFIX = process.env.S3_IMAGE_KEY_PREFIX || null;
 export const S3_VIDEO_KEY_PREFIX = process.env.S3_VIDEO_KEY_PREFIX || null;
-export const FILES_EXPIRATION = parseInt(process.env.FILES_EXPIRATION || '60');
+export const FILES_EXPIRATION = parseInt(process.env.FILES_EXPIRATION) || 604800;
