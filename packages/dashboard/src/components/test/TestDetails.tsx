2fd6027040333ebc6a4273c9c4534d3a6289b1b1
Hackatosh <edouard.benauw@student.ecp.fr>
2020-09-24 13:53:41 +0200

    Added comments

--- packages/dashboard/src/components/test/TestDetails.tsx
+++ packages/dashboard/src/components/test/TestDetails.tsx
@@ -13,6 +13,8 @@ export function TestDetails({
   screenshots,
 }: TestDetailsProps): React.ReactNode {
   const screenshot = screenshots.find((s) => s.testId === test.testId);
+  console.log(screenshot);
+  console.log(screenshots);
   const { css } = useCss();
   const title = test.title.join(' > ');
   return (
