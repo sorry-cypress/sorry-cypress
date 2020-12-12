7ac6a3e7c4c723c00b63fba9612d7b4e606da42e
juanjose.perez <juanjose.perez@geniussportsmedia.com>
2020-12-12 12:18:59 -0500

    add cos driver hbm support

--- packages/director/src/screenshots/cos/config.ts
+++ packages/director/src/screenshots/cos/config.ts
@@ -3,6 +3,7 @@ export const COS_APIKEY = process.env.COS_APIKEY || null;
 export const COS_SERVICEINSTANCE = process.env.COS_SERVICEINSTANCE || null;
 export const COS_REGION = process.env.COS_REGION || 'us-south' ;
 export const COS_BUCKET = process.env.COS_BUCKET || null ;
+export const COS_READ_ENDPOINT = process.env.COS_READ_ENDPOINT || null;
 export const S3_READ_URL_PREFIX = process.env.S3_READ_URL_PREFIX || null;
 export const S3_IMAGE_KEY_PREFIX = process.env.S3_IMAGE_KEY_PREFIX || null;
 export const S3_VIDEO_KEY_PREFIX = process.env.S3_VIDEO_KEY_PREFIX || null;
--- packages/director/src/screenshots/cos/cos.ts
+++ packages/director/src/screenshots/cos/cos.ts
@@ -8,6 +8,7 @@ import {
   S3_IMAGE_KEY_PREFIX,
   S3_VIDEO_KEY_PREFIX,
   COS_BUCKET,
+  COS_READ_ENDPOINT
 } from './config';
 import { S3SignedUploadResult } from './types';
 import { AssetUploadInstruction } from '@src/types';
@@ -22,6 +23,13 @@ const s3 = new ibm.S3({
   endpoint: BUCKET_URL,
   apiKeyId: COS_APIKEY,
   serviceInstanceId: COS_SERVICEINSTANCE,
+  /*cos_hmac_keys: {
+        access_key_id: "1ac9fc5515ca41f781da5cc5655a0c0f",
+        secret_access_key: "2742386b67f671af23906b657ae746b39ef206f9ae85ff24"
+     },*/
+      // these two are required to generate presigned URLs
+  credentials: new ibm.Credentials('1ac9fc5515ca41f781da5cc5655a0c0f', '2742386b67f671af23906b657ae746b39ef206f9ae85ff24', null),
+  signatureVersion: 'v4'
 });
 
 interface GetUploadURLParams {
@@ -43,24 +51,27 @@ export const getUploadUrl = async ({
   };
 
   return new Promise((resolve, reject) => {
-      return resolve({
+     /* return resolve({
         readUrl: `${S3_READ_URL_PREFIX || BUCKET_URL}/${key}`,
         uploadUrl,
-      });
+      });*/
 
-    // s3.getSignedUrl(
-    //   'putObject',
-    //   s3Params,
-    //   (error: Error, uploadUrl: string) => {
-    //     if (error) {
-    //       return reject(error);
-    //     }
-    //     return resolve({
-    //       readUrl: `${S3_READ_URL_PREFIX || BUCKET_URL}/${key}`,
-    //       uploadUrl,
-    //     });
-    //   }
-    // );
+      console.log("Trying get signed URL");
+      console.log("Sengind with: "+JSON.stringify(s3Params));
+    s3.getSignedUrl(
+      'putObject',
+      s3Params,
+      (error: Error, uploadUrl: string) => {
+        if (error) {
+          console.log("errors from signed request: "+error)
+          return reject(error);
+        }
+        return resolve({
+          readUrl: `${COS_READ_ENDPOINT || BUCKET_URL}/${key}`,
+          uploadUrl,
+        });
+      }
+    );
   });
 };
 
