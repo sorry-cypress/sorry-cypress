a5f2e2fd0e856acb4e75cf1dba0dba701b48fab8
juanjopb <juanjopb@hotmail.com>
2020-12-13 11:35:02 -0500

    add cos ibm support driver

--- packages/director/src/screenshots/cos/config.ts
+++ packages/director/src/screenshots/cos/config.ts
@@ -1,9 +1,6 @@
-export const COS_ENDPOINT = process.env.COS_ENDPOINT || null;
-export const COS_APIKEY = process.env.COS_APIKEY || null;
-export const COS_SERVICEINSTANCE = process.env.COS_SERVICEINSTANCE || null;
+export const COS_ACCESSKEY = process.env.COS_ACCESSKEY || null;
+export const COS_SECRETKEY = process.env.COS_SECRETKEY || null;
 export const COS_REGION = process.env.COS_REGION || 'us-south' ;
 export const COS_BUCKET = process.env.COS_BUCKET || null ;
-export const COS_READ_ENDPOINT = process.env.COS_READ_ENDPOINT || null;
-export const S3_READ_URL_PREFIX = process.env.S3_READ_URL_PREFIX || null;
-export const S3_IMAGE_KEY_PREFIX = process.env.S3_IMAGE_KEY_PREFIX || null;
-export const S3_VIDEO_KEY_PREFIX = process.env.S3_VIDEO_KEY_PREFIX || null;
+export const COS_IMAGE_KEY_PREFIX = process.env.COS_IMAGE_KEY_PREFIX || null;
+export const COS_VIDEO_KEY_PREFIX = process.env.COS_VIDEO_KEY_PREFIX || null;
--- packages/director/src/screenshots/cos/cos.ts
+++ packages/director/src/screenshots/cos/cos.ts
@@ -1,34 +1,25 @@
 import ibm from 'ibm-cos-sdk';
 import {
-  COS_ENDPOINT,
-  COS_APIKEY,
-  COS_SERVICEINSTANCE,
+  COS_ACCESSKEY,
+  COS_SECRETKEY,
   COS_REGION,
-  S3_READ_URL_PREFIX,
-  S3_IMAGE_KEY_PREFIX,
-  S3_VIDEO_KEY_PREFIX,
   COS_BUCKET,
-  COS_READ_ENDPOINT
+  COS_IMAGE_KEY_PREFIX,
+  COS_VIDEO_KEY_PREFIX
 } from './config';
 import { S3SignedUploadResult } from './types';
 import { AssetUploadInstruction } from '@src/types';
 import { sanitizeS3KeyPrefix } from './utils';
 
-const BUCKET_URL = COS_ENDPOINT;
+const BUCKET_URL = `https://s3.${COS_REGION}.cloud-object-storage.appdomain.cloud`;
+const COS_READ_ENDPOINT = `https://${COS_BUCKET}.s3.${COS_REGION}.cloud-object-storage.appdomain.cloud`;
 const ImageContentType = 'image/png';
 const VideoContentType = 'video/mp4';
 
 
 const s3 = new ibm.S3({
   endpoint: BUCKET_URL,
-  apiKeyId: COS_APIKEY,
-  serviceInstanceId: COS_SERVICEINSTANCE,
-  /*cos_hmac_keys: {
-        access_key_id: "1ac9fc5515ca41f781da5cc5655a0c0f",
-        secret_access_key: "2742386b67f671af23906b657ae746b39ef206f9ae85ff24"
-     },*/
-      // these two are required to generate presigned URLs
-  credentials: new ibm.Credentials('1ac9fc5515ca41f781da5cc5655a0c0f', '2742386b67f671af23906b657ae746b39ef206f9ae85ff24', null),
+  credentials: new ibm.Credentials(COS_ACCESSKEY, COS_SECRETKEY, null),
   signatureVersion: 'v4'
 });
 
@@ -51,11 +42,6 @@ export const getUploadUrl = async ({
   };
 
   return new Promise((resolve, reject) => {
-     /* return resolve({
-        readUrl: `${S3_READ_URL_PREFIX || BUCKET_URL}/${key}`,
-        uploadUrl,
-      });*/
-
       console.log("Trying get signed URL");
       console.log("Sengind with: "+JSON.stringify(s3Params));
     s3.getSignedUrl(
@@ -79,14 +65,14 @@ export const getImageUploadUrl = async (
   key: string
 ): Promise<AssetUploadInstruction> =>
   getUploadUrl({
-    key: `${sanitizeS3KeyPrefix(S3_IMAGE_KEY_PREFIX)}${key}.png`,
+    key: `${sanitizeS3KeyPrefix(COS_IMAGE_KEY_PREFIX)}${key}.png`,
   });
 
 export const getVideoUploadUrl = async (
   key: string
 ): Promise<AssetUploadInstruction> =>
   getUploadUrl({
-    key: `${sanitizeS3KeyPrefix(S3_VIDEO_KEY_PREFIX)}${key}.mp4`,
+    key: `${sanitizeS3KeyPrefix(COS_VIDEO_KEY_PREFIX)}${key}.mp4`,
     ContentType: VideoContentType,
     Expires: 90,
   });
