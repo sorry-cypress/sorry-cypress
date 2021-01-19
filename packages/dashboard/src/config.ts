f18c820e543353bcb21223cb894415c510f01fae
Fernando Maia <fernando.maia@ifood.com.br>
2021-01-19 12:23:28 -0300

    feat: add support to ApolloClient credentials

--- packages/dashboard/src/config.ts
+++ packages/dashboard/src/config.ts
@@ -1,4 +1,4 @@
 import 'dotenv/config';
 
-export const API_CLIENT_CREDENTIALS =
-  process.env.API_CLIENT_CREDENTIALS || undefined;
+export const GRAPHQL_CLIENT_CREDENTIALS =
+  process.env.GRAPHQL_CLIENT_CREDENTIALS || undefined;
--- packages/dashboard/src/lib/apolloClient.ts
+++ packages/dashboard/src/lib/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { environment } from '../state/environment';
 import { navStructure } from './navigation';
-import { API_CLIENT_CREDENTIALS } from '@src/config';
+import { GRAPHQL_CLIENT_CREDENTIALS } from '@src/config';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -22,13 +22,13 @@ const cache = new InMemoryCache({
 
 const link = createHttpLink({
   uri: environment.GRAPHQL_SCHEMA_URL,
+  credentials: GRAPHQL_CLIENT_CREDENTIALS,
 });
 
 export const client = new ApolloClient({
   cache,
   link,
   resolvers: {},
-  credentials: API_CLIENT_CREDENTIALS,
 });
 
 client.onResetStore(async () => navStructure([]));
