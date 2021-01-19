d8be27a5ec3919ebd21d6a9481896d641f395023
Hackatosh <edouard.benauw@student.ecp.fr>
2021-01-19 17:36:14 +0100

    Add health check to API

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,4 +1,5 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
 import { PORT } from './config';
 import { InstancesAPI } from './datasources/instances';
 import { ProjectsAPI } from './datasources/projects';
@@ -6,6 +7,9 @@ import { RunsAPI } from './datasources/runs';
 import { SpecsAPI } from './datasources/specs';
 import { resolvers } from './resolvers';
 import { typeDefs } from './schema/schema';
+import { getExecutionDriver } from "@sorry-cypress/director/src/drivers";
+import { app } from "@sorry-cypress/director/src/app";
+import { pingDB } from "@src/lib/mongo";
 
 async function start() {
   const dataSources = {
@@ -21,14 +25,21 @@ async function start() {
     dataSources: () => dataSources,
   });
 
-  server
-    .listen({ port: PORT })
-    .then(({ url }) => {
-      console.log(`🚀 Apollo server is ready at ${url}`);
-    })
-    .catch((error) => {
-      throw error;
-    });
+  const app = express();
+
+  app.get('/health-check-mongo', async (_, res) => {
+    await pingDB() ?
+      res.sendStatus(200) :
+      res.sendStatus(503);
+  });
+
+  server.applyMiddleware({ app });
+
+  app.listen({ port: PORT }, () => {
+    console.log(`🚀 Apollo server is listening on port ${PORT}`);
+  }).on('error', (error) => {
+    throw error
+  });
 }
 
 start().catch((error) => {
