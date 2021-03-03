3f62835a92592bac4afeebda2d3938f10e07efc4
Andrew Goldis <agoldis@users.noreply.github.com>
2021-03-03 00:29:40 -0800

    Closes #280 (#288)

--- packages/director/src/lib/results.ts
+++ packages/director/src/lib/results.ts
@@ -1,9 +1,16 @@
 import { InstanceResult } from '@src/types';
-import { deepTraverseKeys } from './lang';
 export const isInstanceFailed = (results: InstanceResult) =>
   results.stats.failures > 0;
 
+function sanitizeKey(_: string, value: any) {
+  if (value && typeof value === 'object') {
+    const replacement: Record<string, any> = {};
+    for (const key in value) {
+      replacement[key.replace('$', '_').replace('.', '_')] = value[key];
+    }
+    return replacement;
+  }
+  return value;
+}
 export const getSanitizedMongoObject = (target: any) =>
-  deepTraverseKeys(target, (key: string) =>
-    key.replace('$', '_').replace('.', '_')
-  );
+  JSON.parse(JSON.stringify(target, sanitizeKey));
