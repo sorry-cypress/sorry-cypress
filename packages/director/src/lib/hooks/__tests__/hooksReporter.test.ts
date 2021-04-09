e4833cabe066e2518430f2d42d66c9d3ec527e6c
Denis Domanskii <denidoman@outlook.com>
2021-04-09 20:04:29 +0300

    unit tests for Slack reporter functionality added

--- packages/director/src/lib/hooks/__tests__/hooksReporter.test.ts
+++ packages/director/src/lib/hooks/__tests__/hooksReporter.test.ts
@@ -3,36 +3,35 @@ import {
   GithubHook,
   HookEvent,
   RunSummary,
+  SlackHook,
 } from '@sorry-cypress/common';
 import axios from 'axios';
 import { reportStatusToBitbucket } from '../reporters/bitbucket';
 import { reportStatusToGithub } from '../reporters/github';
+import { reportToSlack } from '../reporters/slack';
 import bitbucketHook from './fixtures/bitbucketHook.json';
 import bitbucketReportStatusRequest from './fixtures/bitbucketReportStatusRequest.json';
 import githubHook from './fixtures/githubHooks.json';
 import githubReportStatusRequest from './fixtures/githubReportStatusRequest.json';
 import runSummary from './fixtures/runSummary.json';
+import slackHook from './fixtures/slackHook.json';
+import slackReportStatusRequest from './fixtures/slackReportStatusRequest.json';
 
 jest.mock('axios');
 
-const ghHook = ({
-  ...githubHook,
-  url: 'https://github.com/test-company/test-project/',
-} as unknown) as GithubHook;
+beforeEach(() => {
+  ((axios as unknown) as jest.Mock).mockResolvedValueOnce({ status: 200 });
+});
 
-const bbHook = ({
-  ...bitbucketHook,
-  url: 'https://bitbucket.org/testcompany/testrepo.git',
-} as unknown) as BitBucketHook;
+afterEach(() => {
+  jest.restoreAllMocks();
+});
 
-describe('Report Status to Github / Bitbucket', () => {
-  beforeEach(() => {
-    ((axios as unknown) as jest.Mock).mockResolvedValueOnce({ status: 200 });
-  });
-
-  afterEach(() => {
-    jest.restoreAllMocks();
-  });
+describe('Report status to GitHub', () => {
+  const ghHook = ({
+    ...githubHook,
+    url: 'https://github.com/test-company/test-project/',
+  } as unknown) as GithubHook;
 
   it('should send correct request to the github.com repo when run is started', async () => {
     await reportStatusToGithub({
@@ -68,6 +67,13 @@ describe('Report Status to Github / Bitbucket', () => {
         'https://gh.testcompany.com/api/v3/repos/test-company/test-project/statuses/testCommitSha',
     });
   });
+});
+
+describe('Report status to BitBucket', () => {
+  const bbHook = ({
+    ...bitbucketHook,
+    url: 'https://bitbucket.org/testcompany/testrepo.git',
+  } as unknown) as BitBucketHook;
 
   it('should send correct request to the bitbucket.org repo when run is started', async () => {
     await reportStatusToBitbucket({
@@ -82,6 +88,36 @@ describe('Report Status to Github / Bitbucket', () => {
       ...bitbucketReportStatusRequest,
       url:
         'https://api.bitbucket.org/2.0/repositories/testcompany/testrepo/commit/testCommitSha/statuses/build',
+    });
+  });
+});
+
+describe('Report status to Slack', () => {
+  const slkHook = ({
+    ...slackHook,
+    url: 'https://hooks.slack.com/services/123/XXX/zzz',
+    hookEvents: ['RUN_FINISH'],
+    slackResultFilter: 'ONLY_SUCCESSFUL',
+    slackBranchFilter: ['testBranch']
+  } as unknown) as SlackHook;
+
+  it('should send correct request to the Slack when run is finished', async () => {
+    await reportToSlack({
+      hook: slkHook,
+      runId: 'testRunId',
+      ciBuildId: 'testCiBuildId',
+      runSummary: (runSummary as unknown) as RunSummary,
+      hookEvent: HookEvent.RUN_FINISH,
+      commit: {
+        sha: 'testSha123',
+        branch: 'testBranch',
+        message: 'testMessage',
+      },
+    });
+
+    expect(axios).toBeCalledWith({
+      ...slackReportStatusRequest,
+      url: 'https://hooks.slack.com/services/123/XXX/zzz',
     });
   });
 });
