959ba72c0c62b758a594a3a416fccc5443e6466c
Tom Hudson <tom.hudson@igloo.energy>
2021-04-23 11:43:20 +0100

    feature: support monorepos for BitBucket hooks

--- packages/director/src/lib/hooks/reporters/bitbucket.ts
+++ packages/director/src/lib/hooks/reporters/bitbucket.ts
@@ -24,7 +24,7 @@ export async function reportStatusToBitbucket({
 
   const data = {
     state: 'INPROGRESS',
-    key: sha,
+    key: hook.hookId,
     name: hook.bitbucketBuildName || 'sorry-cypress',
     url: getDashboardRunURL(runId),
   };
