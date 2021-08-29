98849c02f24b163da1075d9543ffb97ec46789d8
Andrew Goldis <agoldis@gmail.com>
2021-08-28 22:25:52 -0700

    feat: add mongodb TLS setting. Closes #410

--- packages/mongo/src/config.ts
+++ packages/mongo/src/config.ts
@@ -5,5 +5,6 @@ export const MONGODB_URI =
 export const MONGODB_DATABASE = process.env.MONGODB_DATABASE || 'sorry-cypress';
 export const MONGODB_USER = process.env.MONGODB_USER || undefined;
 export const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD || undefined;
+export const MONGODB_TLS = process.env.MONGODB_TLS || undefined;
 export const MONGODB_AUTH_MECHANISM =
   process.env.MONGODB_AUTH_MECHANISM || undefined;
