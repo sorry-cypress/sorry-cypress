ea2eacc556471c02310379d428d04ada44876f49
Andrew Goldis <agoldis@gmail.com>
2021-09-20 15:11:08 -0700

    feat: rearrange "new project" items

--- packages/dashboard/src/dashboard/dashboardView.tsx
+++ packages/dashboard/src/dashboard/dashboardView.tsx
@@ -1,5 +1,4 @@
 import { Button, Grid } from '@material-ui/core';
-import { Add } from '@material-ui/icons';
 import { SearchField } from '@sorry-cypress/dashboard/components';
 import { WithMaterial } from '@sorry-cypress/dashboard/lib/material';
 import { setNav } from '@sorry-cypress/dashboard/lib/navigation';
@@ -16,32 +15,30 @@ export function DashboardView() {
 
   return (
     <WithMaterial>
-      <Grid
-        container
-        spacing={3}
-        direction="row"
-        alignItems="center"
-        justifyContent="space-between"
-      >
-        <Grid item xs={10}>
-          <SearchField
-            placeholder="Enter project id"
-            onSearch={(value) => setSearch(value)}
-          />
-        </Grid>
-        <Grid item xs={2}>
+      <Grid container spacing={3} direction="row" alignItems="center">
+        <Grid
+          style={{ justifyContent: 'flex-end', display: 'flex' }}
+          item
+          xs={12}
+          justifyContent="flex-end"
+        >
           <Button
             disableElevation
             variant="contained"
             color="primary"
-            size="large"
+            size="small"
             onClick={() => {
               history.push('/--create-new-project--/edit');
             }}
-            startIcon={<Add />}
           >
-            New Project
+            Add New Project
           </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <SearchField
+            placeholder="Enter project id"
+            onSearch={(value) => setSearch(value)}
+          />
         </Grid>
       </Grid>
       <ProjectsList search={search} />
