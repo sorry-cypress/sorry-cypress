19e5729ca6521224409691495f675f3ee8fdd40f
Dmitriy Loshakov <dmitriy@reonomy.com>
2022-01-27 18:09:26 +0000

    Add HOST parameter to API config.

--- packages/api/src/config.ts
+++ packages/api/src/config.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
 
+export const HOST = process.env.HOST || 'localhost';
 export const PORT = process.env.PORT || 4000;
 export const PAGE_ITEMS_LIMIT = Number(process.env.PAGE_ITEMS_LIMIT ?? 20);
