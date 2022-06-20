c6f2b2f25463d7aed77a260610a29d2866550e21
Hackatosh <edouard.benauw@student.ecp.fr>
2022-06-20 19:04:23 +0200

    Add Azure Blob Storage support

---
+++ packages/director/src/screenshots/azure-blob-storage.driver.ts
@@ -0,0 +1,56 @@
+import {
+  AssetUploadInstruction,
+  InstanceResult,
+  Screenshot,
+  ScreenshotUploadInstruction,
+} from '@sorry-cypress/common';
+import { isInstanceFailed } from '@sorry-cypress/director/lib/results';
+import { ScreenshotsDriver } from '@sorry-cypress/director/types';
+import md5 from 'md5';
+import {
+  getImageUploadUrl,
+  getVideoUploadUrl as s3getVideoUploadUrl,
+} from './azure-blob-storage';
+
+const getScreenshotUploadInstruction = (namespace: string) => async (
+  screenshot: Screenshot
+): Promise<ScreenshotUploadInstruction> => {
+  const key = md5(`${namespace}:${screenshot.screenshotId}`);
+  return {
+    ...(await getImageUploadUrl(key)),
+    screenshotId: screenshot.screenshotId,
+  };
+};
+
+export const getVideoUploadUrl = async (
+  instanceId: string,
+  result: InstanceResult
+): Promise<AssetUploadInstruction | null> => {
+  if (!result.cypressConfig?.video) {
+    return null;
+  }
+  if (!isInstanceFailed(result) && !result.cypressConfig.videoUploadOnPasses) {
+    return null;
+  }
+  return await s3getVideoUploadUrl(instanceId);
+};
+
+export const getScreenshotsUploadUrls = async (
+  instanceId: string,
+  result: InstanceResult
+): Promise<ScreenshotUploadInstruction[]> => {
+  if (result.screenshots.length === 0) {
+    return [];
+  }
+
+  return Promise.all(
+    result.screenshots.map(getScreenshotUploadInstruction(instanceId))
+  );
+};
+
+export const driver: ScreenshotsDriver = {
+  id: 'azure-blob-storage',
+  init: () => Promise.resolve(),
+  getScreenshotsUploadUrls,
+  getVideoUploadUrl,
+};
---
+++ packages/director/src/screenshots/azure-blob-storage/azure-blob-storage.ts
@@ -0,0 +1,54 @@
+import {
+  BlobSASPermissions,
+  BlobServiceClient,
+} from '@azure/storage-blob';
+import { AssetUploadInstruction } from '@sorry-cypress/common';
+import {
+  AZURE_CONTAINER_NAME,
+  AZURE_CONNEXION_STRING,
+  AZURE_UPLOAD_URL_EXPIRY_IN_HOURS,
+} from '@sorry-cypress/director/screenshots/azure-blob-storage/config';
+
+const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_CONNEXION_STRING);
+
+interface GetUploadURLParams {
+  key: string;
+}
+
+export const getUploadUrl = async ({
+                                     key,
+                                   }: GetUploadURLParams): Promise<AssetUploadInstruction> => {
+  const signedUploadUrlStartsOn = new Date();
+  const signedUploadUrlExpiresOn = new Date(
+  signedUploadUrlStartsOn.getTime() +
+  3600 * 1000 * AZURE_UPLOAD_URL_EXPIRY_IN_HOURS
+  );
+  const signedReadUrlStartsOn = new Date();
+  const signedReadUrlExpiresOn = new Date(
+  signedUploadUrlStartsOn.getTime() +
+  3600 * 1000 * 24 * 365 // Maximal duration for a SAS is 365 days. We can assume that no one will try to read a snapshot older than a year ago.
+  );
+
+  const uploadUrl = await blobServiceClient.getContainerClient(AZURE_CONTAINER_NAME).getBlobClient(key).generateSasUrl({
+    permissions: BlobSASPermissions.parse('w'),
+    startsOn: signedUploadUrlStartsOn,
+    expiresOn: signedUploadUrlExpiresOn,
+  });
+  const readUrl = await blobServiceClient.getContainerClient(AZURE_CONTAINER_NAME).getBlobClient(key).generateSasUrl({
+    permissions: BlobSASPermissions.parse('r'),
+    startsOn: signedReadUrlStartsOn,
+    expiresOn: signedReadUrlExpiresOn,
+  })
+  return { uploadUrl, readUrl };
+};
+
+export const getImageUploadUrl = async (
+key: string
+): Promise<AssetUploadInstruction> => getUploadUrl({ key: `${key}.png` });
+
+export const getVideoUploadUrl = async (
+key: string
+): Promise<AssetUploadInstruction> =>
+getUploadUrl({
+  key: `${key}.mp4`,
+});
