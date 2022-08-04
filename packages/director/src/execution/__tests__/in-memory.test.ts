63ef1c8d26b3808af30867e442eb9c1d625f09c4
Alyssa <alyssa.burlton@glean.co>
2022-08-04 16:40:44 +0100

    Fix hooksReporter tests

--- packages/director/src/execution/__tests__/in-memory.test.ts
+++ packages/director/src/execution/__tests__/in-memory.test.ts
@@ -6,9 +6,16 @@ const ALL_SPECS = ['one.spec.ts', 'two.spec.ts', 'three.spec.ts']
 it('should report the correct number of claimed specs as they are picked up', async () => {
     const createRunParams = makeCreateRunParameters(ALL_SPECS)
     const { groupId, machineId, runId } = await driver.createRun(createRunParams)
-    const taskResponse = await driver.getNextTask({ groupId, machineId, runId, cypressVersion: '10.3.0' })
-    expect(taskResponse.claimedInstances).toBe(1)
-    expect(taskResponse.totalInstances).toBe(3)
+    const nextTaskRequest = { groupId, machineId, runId, cypressVersion: '10.3.0' }
+    const firstResponse = await driver.getNextTask(nextTaskRequest)
+    expect(firstResponse.claimedInstances).toBe(1)
+    expect(firstResponse.totalInstances).toBe(3)
+
+    const secondResponse = await driver.getNextTask(nextTaskRequest)
+    expect(secondResponse.claimedInstances).toBe(2)
+
+    const thirdResponse = await driver.getNextTask(nextTaskRequest)
+    expect(thirdResponse.claimedInstances).toBe(3)
 })
 
 const makeCreateRunParameters = (specs: string[]): CreateRunParameters => ({
