c3675e05fef18c1aa12af70ddaf19ea8663f9779
bjartur.thorhallsson <bjartur.thorhallsson@controlant.com>
2022-08-17 13:33:46 +0000

    refactoring tests

--- packages/director/src/execution/__tests__/mongo.test.ts
+++ packages/director/src/execution/__tests__/mongo.test.ts
@@ -1,23 +1,5 @@
-import { CreateRunParameters, Run } from '@sorry-cypress/common';
+import { Run } from '@sorry-cypress/common';
 import { driver } from '@sorry-cypress/director/execution/mongo/driver';
-
-const makeCreateRunParameters = (): CreateRunParameters => ({
-  ciBuildId: 'buildId',
-  commit: {
-    sha: '1234',
-    remoteOrigin: 'https://gitlab-ci-token:token-to-remove@gitlab.com',
-  },
-  projectId: 'myProject',
-  specs: ['one.spec.ts'],
-  ci: {
-    params: { ciBuildId: 'buildId' },
-    provider: 'provider',
-  },
-  platform: {
-    osName: 'ubuntu',
-    osVersion: '20.04',
-  },
-});
 
 const mockInsertOneRun = jest.fn(() => {
   return {
@@ -30,7 +12,7 @@ const mockInsertOneRun = jest.fn(() => {
   };
 });
 
-jest.mock('@sorry-cypress/mongo/dist', () => {
+jest.mock('@sorry-cypress/mongo', () => {
   return {
     Collection: {
       project: jest.fn(() => {
@@ -53,11 +35,26 @@ jest.mock('@sorry-cypress/mongo/dist', () => {
 
 describe('runs', () => {
   it('removes gitlab_ci_token from remoteOrigin', async () => {
-    const createRunParams = makeCreateRunParameters();
-    await driver.createRun(createRunParams);
+    await driver.createRun({
+      ciBuildId: 'buildId',
+      commit: {
+        sha: '1234',
+        remoteOrigin: 'https://gitlab-ci-token:token-to-remove@gitlab.com',
+      },
+      projectId: 'myProject',
+      specs: ['one.spec.ts'],
+      ci: {
+        params: { ciBuildId: 'buildId' },
+        provider: 'provider',
+      },
+      platform: {
+        osName: 'ubuntu',
+        osVersion: '20.04',
+      },
+    });
 
     expect(
       (mockInsertOneRun.mock.calls[0][0] as Run).meta.commit.remoteOrigin
-    ).toEqual('gitlab.com');
+    ).toBe('gitlab.com');
   });
 });
