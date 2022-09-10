8f0284543f3afdacc4b3765f427ad0888bf658b0
Math Paquette <me@mathpaquette.com>
2022-09-09 21:03:06 -0400

    feat(dashboard): improve dashboardView

--- packages/dashboard/src/dashboard/dashboardView.tsx
+++ packages/dashboard/src/dashboard/dashboardView.tsx
@@ -1,5 +1,6 @@
-import { Typography } from '@mui/material';
+import { Avatar, Box, Container, Typography } from '@mui/material';
 import { setNav } from '@sorry-cypress/dashboard/lib/navigation';
+import logoDark from '@sorry-cypress/dashboard/resources/logo-dark.svg';
 import React, { FunctionComponent, useLayoutEffect } from 'react';
 import { Navigate, useSearchParams } from 'react-router-dom';
 
@@ -13,16 +14,31 @@ export const DashboardView: DashboardComponent = () => {
 
   return (
     <>
-      <Typography
-        component="h1"
-        variant="h6"
-        color="text.primary"
-        sx={{ mb: 5 }}
-      >
-        Home
-      </Typography>
+      <Container sx={{ display: 'flex', flexDirection: 'column' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="text.primary"
+            sx={{ mb: 5 }}
+          >
+            Welcome to Sorry-Cypress!
+          </Typography>
+        </Box>
 
-      <h3>Welcome to Sorry-Cypress!</h3>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Avatar
+            alt="Sorry Cypress Dashboard Home"
+            src={logoDark}
+            variant="square"
+            sx={{
+              width: '100%',
+              height: '100%',
+              maxWidth: 500,
+            }}
+          />
+        </Box>
+      </Container>
 
       {redirect && <Navigate to="/projects" />}
     </>
