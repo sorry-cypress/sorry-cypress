bb798c29c1206f11b174a740265ab16b06b66bcc
Andrew Goldis <agoldis@gmail.com>
2022-09-16 23:07:34 -0700

    chore: change dev port [skip ci]

--- packages/dashboard/src/lib/github.ts
+++ packages/dashboard/src/lib/github.ts
@@ -1,8 +1,8 @@
 export const getGithubCommitURL = (repo: string, sha: string) =>
-    handleSshURL(repo.replace(/.git$/, '') + `/commit/${sha}`);
+  handleSshURL(repo.replace(/.git$/, '') + `/commit/${sha}`);
 
 export const getGithubBranchURL = (repo: string, branch: string) =>
-    handleSshURL(repo.replace(/.git$/, '') + `/tree/${branch}`);
+  handleSshURL(repo.replace(/.git$/, '') + `/tree/${branch}`);
 
 export const handleSshURL = (sshUrl: string) =>
-    sshUrl.replace(/git@([\w.]+):(.+)/, 'https://$1/$2');
+  sshUrl.replace(/git@([\w.]+):(.+)/, 'https://$1/$2');
