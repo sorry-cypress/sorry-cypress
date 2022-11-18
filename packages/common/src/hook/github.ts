9006a475e91337bbd87cd8467ee09b4ed02aff04
Martin Parsiegla <mp@rebuy.com>
2022-11-18 17:31:45 +0100

    Use octokit to report status to github

--- packages/common/src/hook/github.ts
+++ packages/common/src/hook/github.ts
@@ -1,9 +1,20 @@
+export const getGithubConfiguration = (url: string) => {
+  const [githubProtocol, restOfGithubUrl] = url.split('://');
+  const [githubDomain, githubProject, githubRepo] = restOfGithubUrl.split('/');
+
+  return { githubDomain, githubProject, githubRepo, githubProtocol };
+};
+
 export const getGithubStatusUrl = (url: string, sha: string) => {
   const GITHUB_COM_DOMAIN = 'github.com';
   const GITHUB_COM_ENDPOINT = 'api.github.com';
 
-  const [githubProtocol, restOfGithubUrl] = url.split('://');
-  const [githubDomain, githubProject, githubRepo] = restOfGithubUrl.split('/');
+  const {
+    githubDomain,
+    githubProject,
+    githubRepo,
+    githubProtocol,
+  } = getGithubConfiguration(url);
   const githubEndpoint =
     githubDomain === GITHUB_COM_DOMAIN
       ? GITHUB_COM_ENDPOINT
