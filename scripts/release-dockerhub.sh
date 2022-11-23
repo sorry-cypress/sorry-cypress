d53eabf9389ce70e7cf725ac86cfc558719fb4eb
Erik Hoglid <erik.hoglid@kratzer-automation.com>
2022-11-23 11:21:44 +0100

    Build to private Docker

--- scripts/release-dockerhub.sh
+++ scripts/release-dockerhub.sh
@@ -88,11 +88,11 @@ fi
 echo 🚀 Releasing tags: $TAGS
 echo ========================
 
-dockerBuild "packages/${service}" "agoldis/sorry-cypress-${service}"
+dockerBuild "packages/${service}" "ehoglid/sorry-cypress-${service}"
 # dockerBuild "packages/api" "agoldis/sorry-cypress-api"
 # dockerBuild "packages/dashboard" "agoldis/sorry-cypress-dashboard"
 
-dockerPush "agoldis/sorry-cypress-${service}"
+dockerPush "ehoglid/sorry-cypress-${service}"
 # dockerPush "agoldis/sorry-cypress-api"
 # dockerPush "agoldis/sorry-cypress-dashboard"
 
