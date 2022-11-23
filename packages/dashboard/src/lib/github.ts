68215439050c79a25ea90b24facf4a4915892c66
Erik Hoglid <erik.hoglid@kratzer-automation.com>
2022-11-23 13:24:52 +0100

    Activate US SSL

--- packages/dashboard/src/lib/github.ts
+++ packages/dashboard/src/lib/github.ts
@@ -1,8 +1,17 @@
-export const getGithubCommitURL = (repo: string, sha: string, useSSL: string = 'true') =>
-handleURL(repo.replace(/.git$/, '') + `/commit/${sha}`, useSSL);
+export const getGithubCommitURL = (
+  repo: string,
+  sha: string,
+  useSSL: string = 'true'
+) => handleURL(repo.replace(/.git$/, '') + `/commit/${sha}`, useSSL);
 
-export const getGithubBranchURL = (repo: string, branch: string, useSSL: string = 'true') =>
-handleURL(repo.replace(/.git$/, '') + `/tree/${branch}`, useSSL);
+export const getGithubBranchURL = (
+  repo: string,
+  branch: string,
+  useSSL: string = 'true'
+) => handleURL(repo.replace(/.git$/, '') + `/tree/${branch}`, useSSL);
 
 export const handleURL = (Url: string, UseSSL: string = 'true') =>
-  Url.replace(/git@([\w.]+):(.+)/, 'http://$1/$2');
+  Url.replace(
+    /git@([\w.]+):(.+)/,
+    UseSSL === 'true' ? 'https://$1/$2' : 'http://$1/$2'
+  );
