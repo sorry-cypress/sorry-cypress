be43138e09fea15461755676e261d9c53acca1f4
zabka.pavol <zabka.pavol@scheidt-bachmann.sk>
2023-06-30 18:20:05 +0200

    feature(director): s3 update, make it compatible with other providers

--- packages/director/src/screenshots/s3/s3.ts
+++ packages/director/src/screenshots/s3/s3.ts
@@ -1,78 +1,64 @@
-import { AssetUploadInstruction } from '@sorry-cypress/common';
-import aws from 'aws-sdk';
-import { sanitizeS3KeyPrefix } from '../utils/';
+import { fromEnv } from "@aws-sdk/credential-providers";
+import {AssetUploadInstruction} from '@sorry-cypress/common';
+import {HttpRequest} from "@aws-sdk/protocol-http";
+import {S3RequestPresigner} from "@aws-sdk/s3-request-presigner";
+import {parseUrl} from "@aws-sdk/url-parser";
+import {formatUrl} from "@aws-sdk/util-format-url";
+import {Hash} from "@aws-sdk/hash-node";
+import {sanitizeS3KeyPrefix} from '../utils/';
 import {
-  S3_ACL,
-  S3_BUCKET,
-  S3_FORCE_PATH_STYLE,
-  S3_IMAGE_KEY_PREFIX,
-  S3_READ_URL_PREFIX,
-  S3_REGION,
-  S3_VIDEO_KEY_PREFIX,
-  UPLOAD_EXPIRY_SECONDS,
+    S3_URL,
+    S3_BUCKET,
+    S3_IMAGE_KEY_PREFIX,
+    S3_REGION,
+    S3_VIDEO_KEY_PREFIX,
+    UPLOAD_EXPIRY_SECONDS
 } from './config';
-import { S3SignedUploadResult } from './types';
-
-const BUCKET_URL = S3_FORCE_PATH_STYLE
-  ? `https://s3.amazonaws.com/${S3_BUCKET}`
-  : `https://${S3_BUCKET}.s3.amazonaws.com`;
+import {S3SignedUploadResult} from './types';
 
 const ImageContentType = 'image/png';
 const VideoContentType = 'video/mp4';
 
-const s3 = new aws.S3({
-  region: S3_REGION,
-  signatureVersion: 'v4',
+const presigner = new S3RequestPresigner({
+    credentials: fromEnv(),
+    region: S3_REGION,
+    sha256: Hash.bind(null, "sha256"),
 });
 
 interface GetUploadURLParams {
-  key: string;
-  ContentType?: string;
-  Expires?: number;
+    key: string;
+    ContentType?: string;
+    Expires?: number;
 }
 
-export const getUploadUrl = async ({
-  key,
-  ContentType = ImageContentType,
-  Expires = UPLOAD_EXPIRY_SECONDS,
+export const getUrls = async ({
+    key,
+    ContentType = ImageContentType,
 }: GetUploadURLParams): Promise<S3SignedUploadResult> => {
-  const s3Params = {
-    Bucket: S3_BUCKET,
-    Key: key,
-    Expires,
-    ContentType,
-    ACL: S3_ACL,
-  };
+    const url = parseUrl(`${S3_URL}/${S3_BUCKET}/${key}`);
 
-  return new Promise((resolve, reject) => {
-    s3.getSignedUrl(
-      'putObject',
-      s3Params,
-      (error: Error, uploadUrl: string) => {
-        if (error) {
-          return reject(error);
-        }
-        return resolve({
-          readUrl: `${S3_READ_URL_PREFIX || BUCKET_URL}/${key}`,
-          uploadUrl,
-        });
-      }
+    const signedGetUrlObject = await presigner.presign(
+        new HttpRequest({...url, method: "GET"})
     );
-  });
+
+    const signedPutUrlObject = await presigner.presign(
+        new HttpRequest({...url, method: "PUT", headers: {'Content-Type': ContentType}}),
+        { expiresIn: UPLOAD_EXPIRY_SECONDS }
+    );
+
+    return {
+        readUrl: formatUrl(signedGetUrlObject),
+        uploadUrl: formatUrl(signedPutUrlObject),
+    };
 };
 
-export const getImageUploadUrl = async (
-  key: string
-): Promise<AssetUploadInstruction> =>
-  getUploadUrl({
-    key: `${sanitizeS3KeyPrefix(S3_IMAGE_KEY_PREFIX)}${key}.png`,
-  });
+export const getImageUploadUrl = async (key: string): Promise<AssetUploadInstruction> =>
+    getUrls({
+        key: `${sanitizeS3KeyPrefix(S3_IMAGE_KEY_PREFIX)}${key}.png`,
+    });
 
-export const getVideoUploadUrl = async (
-  key: string
-): Promise<AssetUploadInstruction> =>
-  getUploadUrl({
-    key: `${sanitizeS3KeyPrefix(S3_VIDEO_KEY_PREFIX)}${key}.mp4`,
-    ContentType: VideoContentType,
-    Expires: UPLOAD_EXPIRY_SECONDS,
-  });
+export const getVideoUploadUrl = async (key: string): Promise<AssetUploadInstruction> =>
+    getUrls({
+        key: `${sanitizeS3KeyPrefix(S3_VIDEO_KEY_PREFIX)}${key}.mp4`,
+        ContentType: VideoContentType,
+    });
