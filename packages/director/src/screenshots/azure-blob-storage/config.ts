5df381481e755dc6e3de0ce322605f08ef0a4110
Andrew Goldis <agoldis@gmail.com>
2023-07-08 23:07:53 -0700

    fix: fix parsing UPLOAD_EXPIRY_SECONDS
    closes #808

--- packages/director/src/screenshots/azure-blob-storage/config.ts
+++ packages/director/src/screenshots/azure-blob-storage/config.ts
@@ -4,3 +4,7 @@ export const AZURE_CONTAINER_NAME =
 export const AZURE_UPLOAD_URL_EXPIRY_IN_HOURS = parseInt(
   process.env.AZURE_UPLOAD_URL_EXPIRY_IN_HOURS || '24'
 );
+
+export const UPLOAD_EXPIRY_SECONDS = Number(
+  process.env.UPLOAD_EXPIRY_SECONhDS || '90'
+);
