aa52e912e445be8eae2d69dd868c3b42b1996c2a
samixchoumi <samixchoumi@users.noreply.github.com>
2023-09-18 18:41:08 +0200

    Issues 841 : Add a background color for each project of /project page (#842)
    * Issues 841 : Add a background color for each project of /project page
    
    - Added specific color instead of the default one (red, pink, etc..) to avoid painful UI for the user.
    - Update projectListItem.tsx & projectList.tsx by renaming method.
    
    * Issues 841 : Add a background color for each project of /project page
    
    - Fix groups error regarding @agoldis fix on #852
    
    ---------
    
    Co-authored-by: sdubernet <sdubernet@smartadserver.com>

--- packages/dashboard/src/project/projectListItem.tsx
+++ packages/dashboard/src/project/projectListItem.tsx
@@ -33,11 +33,56 @@ type ProjectListItemProps = {
   reloadProjects: () => void;
 };
 
+/**
+ * Get tests information data from a given project.
+ * @param project the wanted project.
+ * @return the dataset of all tests status.
+ */
+export function useGetTestsFromProject(project: any) {
+  const projectId = project.projectId;
+  const search = '';
+  const [runsFeed] = useGetRunsFeed({
+    projectId,
+    search,
+  });
+  // We get the last run
+  const lastRun = runsFeed?.runs[0];
+  if (lastRun === undefined) return null;
+  const groups = lastRun?.progress?.groups;
+  if (!groups) return null;
+  return getRunTestsProgress(lastRun?.progress as RunProgress);
+}
+
+/**
+ * Get background color for the project component regarding the status of the test execution.
+ * @param project the wanted project.
+ * @return the wanted value (4 cases : empty project, failing project, flaky project, successful project)
+ */
+function useGetBackgroundColorRegardingTestStatus(
+  project: ArrayItemType<
+    Array<{
+      __typename?: 'Project';
+      projectId: string;
+      projectColor: string | null;
+    }>
+  >
+) {
+  const tests = useGetTestsFromProject(project);
+  // No test executed (empty project)
+  if (tests?.overall === undefined || tests?.overall === 0) return '#E7FFFD';
+  // At least 1 test failed
+  else if (tests?.failures) return '#FFC0C8';
+  // No fail but at least 1 test flaky
+  else if (tests?.flaky) return '#FADAF0';
+  // All tests worked
+  else return '#E0FEE7';
+}
+
 export function ProjectListItem({ project }: ProjectListItemProps) {
   // To fix a bug about project created directly from an execution instead of the dashboard
-  let defaultProjectColor;
-  if (project.projectColor === '') defaultProjectColor = '#3486E3';
-  else defaultProjectColor = project.projectColor ?? '#3486E3';
+  let defaultProjectNameColor;
+  if (project.projectColor === '') defaultProjectNameColor = '#3486E3';
+  else defaultProjectNameColor = project.projectColor ?? '#3486E3';
   return (
     <Link
       component={RouterLink}
@@ -51,15 +96,21 @@ export function ProjectListItem({ project }: ProjectListItemProps) {
         }}
       >
         <CardActionArea>
-          <CardContent>
+          <CardContent
+            sx={{
+              backgroundColor: useGetBackgroundColorRegardingTestStatus(
+                project
+              ),
+            }}
+          >
             <ListItem>
               <ListItemAvatar>
                 <Avatar
                   sx={{
-                    backgroundColor: alpha(defaultProjectColor, 0.1),
+                    backgroundColor: alpha(defaultProjectNameColor, 0.1),
                   }}
                 >
-                  <Book sx={{ color: defaultProjectColor }} />
+                  <Book sx={{ color: defaultProjectNameColor }} />
                 </Avatar>
               </ListItemAvatar>
               <ListItemText
@@ -78,34 +129,12 @@ export function ProjectListItem({ project }: ProjectListItemProps) {
   );
 }
 
-/**
- * Get tests information data from a given project.
- * @param project the wanted project.
- * @return the dataset of all tests status.
- */
-export function getTestsFromProject(project: any) {
-  const projectId = project.projectId;
-  const search = '';
-  const [runsFeed] = useGetRunsFeed({
-    projectId,
-    search,
-  });
-  // We get the last run
-  const lastRun = runsFeed?.runs[0];
-  if (lastRun === undefined) return null;
-
-  const groups = lastRun?.progress?.groups;
-  if (!groups) return null;
-
-  return getRunTestsProgress(lastRun?.progress as RunProgress);
-}
-
 // New component to show the latest run data for a project
 const LastTestRunData: React.FC<{ project: any }> = ({ project }) => {
   if (project === undefined) {
     return null;
   } else {
-    const tests = getTestsFromProject(project);
+    const tests = useGetTestsFromProject(project);
     return (
       <Grid container>
         <Grid item>
