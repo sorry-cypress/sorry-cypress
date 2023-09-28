84cb5d4192761537f4e872b569189e0768027d2b
Damien Cassou <damien@cassou.me>
2023-09-28 16:39:31 +0200

    refactor: extract makeInstance() out of createInstance()
    The former handles instance creation while the latter takes care of
    request/response handling.

--- packages/director/src/api/instances.ts
+++ packages/director/src/api/instances.ts
@@ -25,66 +25,20 @@ export const createInstance: RequestHandler = async (req, res) => {
   const { runId } = req.params;
   const cypressVersion = req.headers['x-cypress-version']?.toString() ?? '';
 
-  const executionDriver = await getExecutionDriver();
-
   getLogger().log(
     { ...req.body, ...req.headers },
     `Machine is requesting a new task`
   );
 
   try {
-    const task = await executionDriver.getNextTask({
-      runId,
+    const instance = await makeInstance(
+      groupId,
       machineId,
-      groupId,
-      cypressVersion,
-    });
-
-    if (task.instance === null) {
-      getLogger().log(
-        { runId, machineId, groupId, task },
-        `All spec files claimed, nothing to run`
-      );
-      return res.json({
-        spec: null,
-        instanceId: null,
-        claimedInstances: task.claimedInstances,
-        totalInstances: task.totalInstances,
-      });
-    }
-
-    emitInstanceStart({
       runId,
-      groupId,
-      spec: task.instance.spec,
-    });
-
-    if (task.claimedInstances === 1) {
-      getLogger().log(
-        {
-          ...task,
-        },
-        'Detected first claimed instance for group'
-      );
-      emitRunStart({
-        runId,
-        groupId,
-        projectId: task.projectId,
-      });
-    }
-
-    //Instance Start
-    getLogger().log(
-      { ...task.instance, machineId, groupId, cypressVersion },
-      `Sending a new instance to machine`
+      cypressVersion
     );
 
-    return res.json({
-      spec: task.instance.spec,
-      instanceId: task.instance.instanceId,
-      claimedInstances: task.claimedInstances,
-      totalInstances: task.totalInstances,
-    });
+    return res.json(instance);
   } catch (error) {
     getLogger().error({ error }, 'Error while creating instance...');
     if (error.code && error.code === RUN_NOT_EXIST) {
@@ -92,6 +46,68 @@ export const createInstance: RequestHandler = async (req, res) => {
     }
     throw error;
   }
+};
+
+export const makeInstance = async (
+  groupId,
+  machineId,
+  runId,
+  cypressVersion
+) => {
+  const executionDriver = await getExecutionDriver();
+
+  const task = await executionDriver.getNextTask({
+    runId,
+    machineId,
+    groupId,
+    cypressVersion,
+  });
+
+  if (task.instance === null) {
+    getLogger().log(
+      { runId, machineId, groupId, task },
+      `All spec files claimed, nothing to run`
+    );
+    return {
+      spec: null,
+      instanceId: null,
+      claimedInstances: task.claimedInstances,
+      totalInstances: task.totalInstances,
+    };
+  }
+
+  emitInstanceStart({
+    runId,
+    groupId,
+    spec: task.instance.spec,
+  });
+
+  if (task.claimedInstances === 1) {
+    getLogger().log(
+      {
+        ...task,
+      },
+      'Detected first claimed instance for group'
+    );
+    emitRunStart({
+      runId,
+      groupId,
+      projectId: task.projectId,
+    });
+  }
+
+  //Instance Start
+  getLogger().log(
+    { ...task.instance, machineId, groupId, cypressVersion },
+    `Sending a new instance to machine`
+  );
+
+  return {
+    spec: task.instance.spec,
+    instanceId: task.instance.instanceId,
+    claimedInstances: task.claimedInstances,
+    totalInstances: task.totalInstances,
+  };
 };
 
 // - /instances/:instanceId/tests before running a spec
